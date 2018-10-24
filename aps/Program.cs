c11aef24b88f1f3c8cd1ec2a35b5d9a31920d725
persio godoy <persiopg@gmail.com>
2018-10-24 11:32:03 -0300

    criaçao do projeto

---
+++ aps/Program.cs
@@ -0,0 +1,12 @@
+﻿ using System;
+
+namespace aps
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
